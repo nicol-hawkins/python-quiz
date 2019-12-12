b18cb94b38bdf1c5d20f964fc9af56667d36303b
Win Naing <wintunnaing55@gmail.com>
2019-12-11 17:39:55 -0800

    adding new api to win branch

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import requests
 
+
+def index(request):
+    response= requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.804363,-122.271111&radius=1500&type=chinesemedicine&keyword=herbshop&key=AIzaSyDOWpD8jNHxjkOrNlI7Coka1wW5NU8-tJU')
+    print(response.status_code)
+    print(response.json())
+
+    if request.method == 'POST':
+       pass
+    # print(request.POST)
+    #     form = CityForm(request.POST)       #for adding into the form
+    #     form.save()
+     
 # Two example views. Change or delete as necessary.
 def home(request):
 
@@ -10,8 +23,32 @@ def home(request):
     return render(request, 'pages/home.html', context)
 
 def about(request):
-    context = {
-    }
+    context = {}
 
     return render(request, 'pages/about.html', context)
+
+def searchherbstore(request):
+    context = {}
+
+    return render(request, 'pages/searchherbstore.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# response = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.804363,-122.271111&radius=1500&type=chinesemedicine&keyword=herbshop&key=AIzaSyDOWpD8jNHxjkOrNlI7Coka1wW5NU8-tJU')
+# data = response.json()
+# print(data)
+
+
 
